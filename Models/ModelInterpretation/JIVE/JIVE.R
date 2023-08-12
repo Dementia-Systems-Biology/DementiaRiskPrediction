df340c8d4ff01658be938a58447c42d43b86e141
jarnokoetsier <jarno.koetsier@gmail.com>
2023-08-12 18:31:11 +0200

    Update JIVE.R

--- Models/ModelInterpretation/JIVE/JIVE.R
+++ Models/ModelInterpretation/JIVE/JIVE.R
@@ -35,48 +35,78 @@ rownames(genoType) <- test[,3]
 # Save genotype data
 save(genoType,file = "EXTEND/Data/genoType.RData")
 
-# Load DNA methylation data
+# Load data
 load("EXTEND/Data/methSet_allNorm_fil.RData")
+load("EXTEND/Data/genoType.RData")
 load("Models/ModelInterpretation/selCpGs.RData")
 
-# Get mQTLs of model's features
-query <- list(
-  cpgs = selCpGs,
-  pval = 1e-5,
-  clumped = 1
-)
-res <- POST("http://api.godmc.org.uk/v0.1/query", body = query, encode = "json")
-test <- content(res) %>% lapply(., as_data_frame) %>% bind_rows
-selSNPs <- unique(str_remove(str_remove(str_remove(test$name, "chr"), ":SNP"), ":INDEL"))
+# Make empty data frame for explained variance
+varExpl <- as.data.frame(matrix(NA,nrow = length(selCpGs),ncol = 2))
+colnames(varExpl) <- c("Methylation", "Genotype")
 
-samples <- intersect(colnames(methSet_allNorm_fil), colnames(genoType))
-methData <- methSet_allNorm_fil[rownames(methSet_allNorm_fil) %in% selCpGs,samples]
-genoType_fil <- genoType[rownames(genoType) %in% selSNPs,samples]
+for (i in 1:length(selCpGs)){
+  
+  # Get mQTLs of model's features
+  selCpGs1 <- selCpGs[[i]]
+  query <- list(
+    cpgs = selCpGs1,
+    pval = 1e-5,
+    clumped = 1
+  )
+  res <- POST("http://api.godmc.org.uk/v0.1/query", body = query, encode = "json")
+  test <- content(res) %>% lapply(., as_data_frame) %>% bind_rows
+  selSNPs <- unique(str_remove(str_remove(str_remove(test$name, "chr"), ":SNP"), ":INDEL"))
+  
+  # Prepare data for JIVE
+  samples <- intersect(colnames(methSet_allNorm_fil), colnames(genoType))
+  methData <- methSet_allNorm_fil[rownames(methSet_allNorm_fil) %in% selCpGs1,samples]
+  genoType_fil <- genoType[rownames(genoType) %in% selSNPs,samples]
+  all(colnames(genoType_fil) == colnames(methData))
+  
+  # Make JIVE list
+  jive_data <- list(Methylation = methData,
+                    Genotype = genoType_fil)
+  
+  # Perform JIVE
+  set.seed(123)
+  jive_results <- jive(jive_data)
+  
+  # Get explained variance
+  varExpl[i,] <- summary(jive_results)$Variance[1,]
+  
+  # Save intermediate results
+  save(varExpl, file = "Models/ModelInterpretation/JIVE/varExpl.RData")
+}
 
-all(colnames(genoType_fil) == colnames(methData))
+# Set rownames
+rownames(varExpl) <- names(selCpGs)
 
-jive_data <- list(Methylation = methData,
-                  Genotype = genoType_fil)
+# Prepare data for plotting
+plotDF <- data.frame(Name = rep(c("Syst. blood pressure",
+                              "Low education",
+                              "Physical inactivity",
+                              "Dietary intake",
+                              "Depression",
+                              "Type II diabetes",
+                              "Sex",
+                              "Age",
+                              "Alcohol consumption",
+                              "HDL cholesterol"),2),
+                     Type = rep(c("Methylation", "Genotype"), each = 10),
+                     Value = c(varExpl[,1], varExpl[,2]))
 
-set.seed(123)
-jive_results <- jive(jive_data)
-save(jive_results, file = "GenoMeth/jive_results_Depression.RData")
+# Make plot
+p <- ggplot(plotDF) +
+  geom_bar(aes(x = Name, y = Value, fill = Type), stat = "identity",
+           position = position_dodge(), color = "black") +
+  coord_flip() +
+  xlab("") +
+  ylab("Joint variation") +
+  scale_fill_manual(values = c("#E6AB02","#EF3B2C")) +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        legend.title = element_blank())
 
-showVarExplained(jive_results)
-summary(jive_results)
-
-
-sum(genoType == "1/2")
-sum(genoType == "2/1")
-sum(genoType == "0/2")
-sum(genoType == "2/0")
-sum(genoType == "2/2")
-
-load("~/Data/methSet_allNorm_fil.RData")
-
-load("~/Data/metaData_ageFil.RData")
-rownames(dat) <- dat$ID
-test <- dat[str_remove(colnames(genoType), "0_"), "Basename"]
-colnames(genoType) <- test
-
+# Save plot
+ggsave(p, file = "Models/ModelInterpretation/JIVE/jointVariation_JIVE.jpg", width = 7.5, height = 5)
 
